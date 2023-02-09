d43067373abc3b4369d7d4728064d31e37292a2d
chiamaka28 <maduikechiamaka4@gmail.com>
2023-02-09 15:38:14 +0100

    challenge 07 done

---
+++ challenge-07/index.js
@@ -0,0 +1,11 @@
+const classroom = {
+    hasTeachingAssistant : true;
+    classList:['rashida', 'john', 'roman', 'lisa', 'omair', 'lukas']
+}
+
+
+const getStudents = (classroom) => {
+    if (classrom.hasTeachingAssistant === true) {
+        let teacher  = 
+    }
+}
